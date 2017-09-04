1c47ea3ef96d2243daea629106022fe37cbd5ab4
Abhinav Neelam <abhinav.neelam@247-inc.com>
2017-09-05 02:44:34 +0530

    Checking in Chinese whisper basic

---
+++ chinese_whisper/whisper_group.erl
@@ -0,0 +1,14 @@
+-module(whisper_group). 
+-export([setup/1]). 
+
+
+setup(Players) when Players>1 -> 
+	LastPlayer=whisper:start(rand:uniform()),
+	setup(Players-1,LastPlayer). 
+
+setup(Players, Neighbour) when Players>=1 -> 
+	CurrentPlayer=whisper:start(rand:uniform(),Neighbour),
+	setup(Players-1,CurrentPlayer);
+setup(0, Player) -> Player. 
+
+
